307c8edcaef41256d98ad080d9dac7b58f619907
ViralL <viirall@yandex.ru>
2015-06-30 00:01:26 +0300

    admin-bar

---
+++ app/scripts/app.js
@@ -0,0 +1,25 @@
+
+$(function() {
+    var padding = 83;
+    $('.page-scroll a').on('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - padding
+        }, 1000);
+        event.preventDefault();
+    });
+});
+$('select').select2({
+    minimumResultsForSearch: Infinity
+});
+$('.sidebar-toggler').click(function() {
+    $('.page-sidebar-menu').toggleClass('page-sidebar-menu-closed');
+    $('.page-sidebar-menu').toggleClass('page-sidebar-menu-open');
+    $('.page-content-wrapper .page-content').toggleClass('page-content-open');
+
+});
+//toggle
+$('.cat__lines__header').click(function(){
+    $(this).parent().find('.cat__lines__body').toggleClass('active');
+    return false;
+});
